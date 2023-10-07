c69e207ecb4c171cc3f8d1219852a3000b1fa222
Anilchauhan24 <rewardpointsbit@gmail.com>
2023-10-07 15:34:31 +0530

    update

---
+++ conditional/assignOperator.js
@@ -0,0 +1,29 @@
+//Assigment operator equal sign + - *% += +-++--/= like multiple sign, addition sign, subsctration sign, modulus sign, 
+//left shift <<=, right shift >>>=, divide sign /=
+
+/*
+let num = 12;
+num = num + 20;
+
+console.log(num);//32
+
+
+
+//comma operator,
+
+let sum1 = 20, sum2 = 10, sum3 = 53.69;
+
+let numb = (4, 5, 6, 7, 8, 0);
+
+console.log(numb); //0 last index
+
+console.log(numb[0])//undefined because of this ();
+*/
+
+let gd = prompt("Please type your name");
+
+let name = document.getElementById('name').innerHTML = gd;
+
+alert(`Thank you for choosing us${gd}`);
+
+
